8bf1edc010b49a9743761a89e4e5ab51a4a151f9
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2021-12-28 11:48:46 +0100

    Dodaj pliki projektów.

---
+++ Zadanie 4/Converters/IntToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Zadanie_4.Converters
+{
+    internal class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null)
+                return System.Convert.ToInt32(value) >= System.Convert.ToInt32(parameter) ? "passed": "Failed";
+            else
+                return System.Convert.ToInt32(value) >= 50 ? "passed" : "Failed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Zadanie 4/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //teraz program działa tylko na podstawie tej wartości Grade ustawionej na początku
+        //TODO z pola textbox uaktualnić pole Grade
+        public int Grade { get; set; } = 80;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
